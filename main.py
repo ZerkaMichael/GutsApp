e9119b2333bd411b797a385fc1021b2499d39cfc
Zerka <zerkamichael@gmail.com>
2022-03-10 04:52:14 -0500

    * Inital round of tournament(Randomized teams paired)
    * Menu to view current matches of tournament

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
 import kivy
+import random
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
+from kivy.properties import ListProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from os import path
@@ -65,31 +66,66 @@ class MatchHistoryWindow(Screen):
         self.ids.match8.text = self.history(8)
 
 class TournamentWindow(Screen):
-	pass
+    teamList = ListProperty(["[None]"])
+    def getMatches(self):
+        with open('currentTournament.json', 'r') as file:
+            data = json.load(file)
+            teamList = []
+            for i in range(len(data["gamedata"]["currentmatches"])):
+                temp = data["gamedata"]["currentmatches"][i]["Text"]
+                teamList.append(temp)
+            self.teamList = teamList
 
 class AdminWindow(Screen):
     def submitTeams(self):
         with open('currentTournament.json', 'r+') as file:
             data = json.load(file)
             newTeam = self.ids.team_input.text
-            data["teams"].append(newTeam)
+            data["gamedata"]["teams"].append(newTeam)
             file.seek(0)
             json.dump(data, file, indent = 4)
 
     def getTeams(self):
         with open('currentTournament.json', 'r') as file:
             data = json.load(file)
-            return data["teams"]
+            return data["gamedata"]["teams"]
 
     def removeTeam(self, team):
         with open('currentTournament.json', 'r+') as file:
             data = json.load(file)
-            if team in data["teams"]:
-                data["teams"].remove(team)
+            if team in data["gamedata"]["teams"]:
+                data["gamedata"]["teams"].remove(team)
                 file.truncate(0)
                 file.seek(0)
                 json.dump(data, file, indent = 4)
-                #self.ids.remove_team.values = data["teams"]
+
+    def startTournament(self):
+        unmatched = []
+        with open('currentTournament.json', 'r+') as file:
+            data = json.load(file)
+            teams = data["gamedata"]["teams"]
+            teamCount = len(teams)
+            unmatched = list(range(0,teamCount))
+            match = 0
+            if (teamCount % 2) == 0:
+                i = 1
+                gamesAmount = teamCount/2
+                while match < gamesAmount:
+                    random.shuffle(unmatched)
+                    selTeam1 = unmatched[0]
+                    selTeam2 = unmatched[1]
+                    team1 = data["gamedata"]["teams"][selTeam1]
+                    team2 = data["gamedata"]["teams"][selTeam2]
+                    #print("Pair {}: {} and {}".format(i, team1, team2))
+                    newMatch = {"Team1": team1, "Team2": team2, "FinalScore": "0-0", "Text": team1+" vs "+team2}
+                    data["gamedata"]["currentmatches"].append(newMatch)
+                    i += 1
+                    match += 1
+                    unmatched.remove(selTeam1)
+                    unmatched.remove(selTeam2)
+                file.truncate(0)
+                file.seek(0)
+                json.dump(data, file, indent = 4)
 
 class WindowManager(ScreenManager):
 	pass
